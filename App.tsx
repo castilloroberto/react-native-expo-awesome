9bb8b7bfbac1f8b8e2ca304ea053152ce3b3f5df
robertocastillo <robertocastillo945@gmail.com>
2021-08-21 14:48:55 -0600

    final

--- App.tsx
+++ App.tsx
@@ -1,12 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import * as Linking from 'expo-linking';
+
 
 export default function App() {
+  const [number, setNumber] = useState('')
+  const handlePress = (tel:string)=>{
+    Linking.openURL('tel:'+tel)
+    
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Make a phone call</Text>
+
+      <View style={styles.inputs}>
+      <TextInput style={styles.phone} value={number} onChangeText={setNumber}/>
+
+      <Button title="Llamar" onPress={()=>handlePress(number)}/> 
+      </View>
     </View>
   );
 }
@@ -18,4 +30,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  inputs:{
+    flexDirection:'row',
+    padding:20,
+    flexGrow:0
+  },
+  phone:{
+    width:200,
+    height:30,
+    backgroundColor:'#ecf0f1',
+    margin:10,
+    padding:5
+  }
 });
